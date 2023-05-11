5293096ccab9a45bebe3577f17b5a7be31523fcf
Adib Hanna <adibhanna@gmail.com>
2023-05-11 12:27:20 -0500

    cleanup

--- init.lua
+++ init.lua
@@ -667,23 +667,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<leader>v', "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
     end,
 })
-
--- -- Run gofmt on save
--- local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*.go",
---   callback = function()
---    require('go.format').gofmt()
---   end,
---   group = format_sync_grp,
--- })
-
--- -- Run gofmt + goimport on save
--- local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   pattern = "*.go",
---   callback = function()
---    require('go.format').goimport()
---   end,
---   group = format_sync_grp,
--- })
